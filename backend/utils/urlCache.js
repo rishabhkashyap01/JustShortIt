98813fd0579d16affb0dbf6d110f21f084665e35
rishabhkashyap01 <rishabhkashyap8135@gmail.com>
2026-05-20 17:08:13 +0530

    add cache

--- backend/utils/urlCache.js
+++ backend/utils/urlCache.js
@@ -1,3 +1,12 @@
 const cache = new Map();
 const hitBuffer = new Map();
 
+const getFromCache = (short_code) => {
+    const result = cache.get(short_code) || null;
+    if(result){
+        hitBuffer.set(short_code, (hitBuffer.get(short_code) || 0)+ 1);
+    }
+    return result;
+};
+
+
